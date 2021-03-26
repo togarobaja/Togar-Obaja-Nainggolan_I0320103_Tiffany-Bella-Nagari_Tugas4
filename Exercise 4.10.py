04106882e0e34f1b1ed0d6025ed2bc95972b0fa1
Togar Obaja <80144717+togarobaja@users.noreply.github.com>
2021-03-26 08:35:07 +0700

    Add files via upload

---
+++ Exercise 4.10.py
@@ -0,0 +1,7 @@
+#string
+str = "Hello World"
+
+
+#skip
+new_str = str[0::2]
+print(new_str)
---
+++ Exercise 4.4.py
@@ -0,0 +1,10 @@
+# strings
+
+str1 = "Hello"
+str2 = "World"
+
+# concate
+result = str1 + " " + str2
+
+# output
+print(result)
